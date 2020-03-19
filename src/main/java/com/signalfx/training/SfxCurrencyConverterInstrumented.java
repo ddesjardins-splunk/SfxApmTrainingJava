1ece539d729e897dd3448ff2f63f5e6e292d9b8d
ddesjardins-splunk <ddesjardins@splunk.com>
2020-03-19 12:23:34 -0600

    fixed SignalFx Auto and Manual instrumented example

--- src/main/java/com/signalfx/training/SfxCurrencyConverterInstrumented.java
+++ src/main/java/com/signalfx/training/SfxCurrencyConverterInstrumented.java
@@ -19,22 +19,22 @@ import javax.money.convert.MonetaryConversions;
 import javax.money.format.MonetaryAmountFormat;
 import javax.money.format.MonetaryFormats;
 
-import com.signalfx.tracing.api.Trace;
-
 import java.util.AbstractMap;
 import java.util.Locale;
 
-  
 import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
+import com.signalfx.tracing.api.Trace;
+
+
 
 
 public class SfxCurrencyConverterInstrumented {
 		
 	private static SfxCurrencyConverterInstrumented converterInstance = new SfxCurrencyConverterInstrumented();
-	private final Tracer tracer = GlobalTracer.get();
+	private static final Tracer s_tracer = GlobalTracer.get();
 
     private static Map<String, String> fromMap = Map.ofEntries(
     		  new AbstractMap.SimpleEntry<String, String>("United states", "USD"),
@@ -61,35 +61,49 @@ public class SfxCurrencyConverterInstrumented {
     	   System.exit(1);
       } else {
         	converterInstance.convertMyAmount(new BigDecimal(args[0]));
+        	// The sleep is here below because the Tracer object is not fully shutdown when the app exits, thus throwing exeception
+        	// in production situations this will not be  the case as this is a short-lived application.
+        	Thread.sleep(3000);
       }   
     }
-    
-    @Trace(operationName = "doConversion")
+   	
+	@Trace(operationName = "doConversion")
     private void doConversion ( BigDecimal amount, String fromCurrency, String fromLocale,  String toCurrency, String toLocale) {
-    	MonetaryAmount fromAmount = Monetary.getDefaultAmountFactory().setCurrency(fromCurrency).setNumber(amount).create();
-		 CurrencyConversion conversion = MonetaryConversions.getConversion(toCurrency);
-		 MonetaryAmount convertedCurrency = fromAmount.with(conversion);
-		 
-		 MonetaryAmountFormat formatUS = MonetaryFormats.getAmountFormat(Locale.US);
-		 String formatted = formatUS.format(convertedCurrency);
-		 
-		 System.out.println(amount + " in " + fromLocale + " (" + fromCurrency + ") is equivalent to " + formatted + " in " + toLocale );	
-    }
 
-    @Trace(operationName = "convertMyAmount")
-    private void convertMyAmount(BigDecimal amount) {
-    	 for (Map.Entry<String,String> from : fromMap.entrySet())  {
-    		 
-    		 //System.out.println("Key = " + from.getKey() + ", Value = " + from.getValue()); 
-    		 
-    		 for (Map.Entry<String,String> to : toMap.entrySet())  {
-    			 //System.out.println("Key = " + to.getKey() +  ", Value = " + to.getValue());
-    			 
-    			 doConversion ( amount, from.getValue(),  from.getKey(), to.getValue(), to.getKey() );
-    		 }
-    	 }
-    }
-    
+   	 final Span span = s_tracer.buildSpan("doConverstion").start();
+   	    try (Scope scope = s_tracer.scopeManager().activate(span)) {
+   	    	span.setTag("Converting "+ amount.toString(),  " From:" + fromLocale + " To:"+toLocale);
+   	        MonetaryAmount fromAmount = Monetary.getDefaultAmountFactory().setCurrency(fromCurrency).setNumber(amount).create();
+   			CurrencyConversion conversion = MonetaryConversions.getConversion(toCurrency);
+   			MonetaryAmount convertedCurrency = fromAmount.with(conversion);
+
+   			 MonetaryAmountFormat formatUS = MonetaryFormats.getAmountFormat(Locale.US);
+   			 String formatted = formatUS.format(convertedCurrency);
+
+   			 System.out.println(amount + " in " + fromLocale + " (" + fromCurrency + ") is equivalent to " + formatted + " in " + toLocale );	
+
+   	    } finally {
+   	       span.finish();
+   	    }
+
+   }
+
+   @Trace(operationName = "convertMyAmount")
+   private void convertMyAmount(BigDecimal amount) {
+
+   	final Span span = s_tracer.buildSpan("convertMyAmount").start();
+   		try (Scope scope = s_tracer.scopeManager().activate(span)) {
+   			for (Map.Entry<String,String> from : fromMap.entrySet())  { 
+   				//System.out.println("Key = " + from.getKey() + ", Value = " + from.getValue()); 
+   				for (Map.Entry<String,String> to : toMap.entrySet())  {
+   					//System.out.println("Key = " + to.getKey() +  ", Value = " + to.getValue());
+   					doConversion ( amount, from.getValue(),  from.getKey(), to.getValue(), to.getKey() );
+   				}
+   			}
+   		} finally {
+   		span.finish();
+   		}
+   }
 }
 
 
