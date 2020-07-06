6b0a5552d039cb666a8caa6ed92b766f30639483
Shabu <ddesjardins@splunk.com>
2020-07-06 10:14:31 -0600

    Added Deadlock scenario to demonstrate JMXDeadLock Monitoring.

--- src/main/java/com/signalfx/training/SfxCurrencyConverterServerAutoJmx.java
+++ src/main/java/com/signalfx/training/SfxCurrencyConverterServerAutoJmx.java
@@ -1,19 +1,32 @@
 package com.signalfx.training;
 
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 
 import javax.management.remote.JMXServiceURL;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.jmx.ConnectorServer;
 import org.eclipse.jetty.jmx.MBeanContainer;
 import org.eclipse.jetty.server.Server;
 
 public class SfxCurrencyConverterServerAutoJmx extends SfxCurrencyConverterServerAuto {
-	 
+	
+	public void handle(String target, org.eclipse.jetty.server.Request baseRequest, HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+	
+		if ( target.equals("/deadlock") ) {
+			DeadLockTest.createDeadlock();
+		}else {
+			super.handle(target, baseRequest, request, response);
+		}
+	}
 	
 	public static void main(String[] args) throws Exception {
 	    	
-		 Server server = new Server(8888);
+		Server server = new Server(8888);
 		
 		// Setup JMX
 		MBeanContainer mbeanContainer = new MBeanContainer(ManagementFactory.getPlatformMBeanServer());
@@ -25,9 +38,9 @@ public class SfxCurrencyConverterServerAutoJmx extends SfxCurrencyConverterServe
 		server.addBean(jmxServer);
 		  
 		 
-	     server.setHandler(new SfxCurrencyConverterServerAuto());
-	     server.start();
-	     server.join();	    	
+	    server.setHandler(new SfxCurrencyConverterServerAutoJmx());
+	    server.start();
+	    server.join();	    	
 	 }
 
 }
